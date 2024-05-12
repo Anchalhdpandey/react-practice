47be04efcdb348b9a764b11ba161a1a014ef712c
Anchalhdpandey <152679483+Anchalhdpandey@users.noreply.github.com>
2024-05-12 22:37:05 +0530

    az

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 
 function Github() {
+  const [data, setData]=useState([])
+  useEffect(()=>{
+    fetch('https://api.github.com/users/Anchalhdpandey')
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data);
+      setData(data)
+    })
+  },[])
   return (
-    <div className='text-center m-4 bg-gray-500 text-white p-4 text-3xl'>Github followers:</div>
+    <div className='text-center m-4 bg-gray-500 text-white p-4 text-3xl'>Github followers:{data.followers}
+    <img src={data.avatar_url} alt='git picture' width={300}/>
+    </div>
   )
 }
 
 export default Github
+export const githubInfoLoader=async()=>{
+  const response=await fetch('https://api.github.com/users/Anchalhdpandey')
+  return response.json()
+}
